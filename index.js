9f52335a58546e14f134bfcd5a8aa36f0ba722ab
Alex Loren <lorenalexm@gmail.com>
2017-03-13 00:48:24 -0700

    Added alexa-sdk package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var nraw = require('nraw')
 var reddit = new nraw('Alexa /r/showerthoughts skill')
+var alexa = require('alexa-sdk')
 
 function getSubredditTitle(callback) {
 	reddit.subreddit('showerthoughts').hot().exec(function(res) {
